a7ff9487b1cf2445c791f1ced2e6eae36a688070
otori334 <41427185+otori334@users.noreply.github.com>
2021-10-24 07:12:09 +0900

    Update wmono.py

--- wmono.py
+++ wmono.py
@@ -9,9 +9,19 @@ import argparse
 parser = argparse.ArgumentParser(description='Script to make wav mono.')
 parser.add_argument('arg1', help='[inputfile]')
 parser.add_argument('arg2', help='[outputfile]')
+parser.add_argument('-c', '--ch', type=int, default=0, choices=range(1,10), help='[select channel]')
 args = parser.parse_args()
 input = os.path.abspath(args.arg1)
 output = os.path.abspath(args.arg2)
+
+# 入力の絶対パスを解析
+if os.path.isfile(input) == False:
+    print("The first argument should be an existing file.")
+    sys.exit()
+input_splitext = os.path.splitext(input)[1]
+if input_splitext != '.wav' and input_splitext != '.WAV':
+    print("The extension of the first argument must be \'.wav\' or \'.WAV\'.")
+    sys.exit()
 
 # waveモジュールで読み込む
 with wave.open(input) as wav:
@@ -22,10 +32,13 @@ with wave.open(input) as wav:
     if nchannels == 1:
         print("wmono.py: Target is already mono.")
         sys.exit()
+    whichchannel = (args.ch - 1) % (nchannels)
+    print(f'Channel {whichchannel + 1} has been selected.')
     if samplewidth == 2:
-        data = np.frombuffer(wav.readframes(nframes), dtype='int16').copy().reshape([nchannels, nframes], order='F').mean(axis=0, dtype = "int16")
+        data = np.frombuffer(wav.readframes(nframes), dtype='int16')[whichchannel::nchannels].copy()
+        #data = np.frombuffer(wav.readframes(nframes), dtype='int16').copy().reshape([nchannels, nframes], order='F').mean(axis=0, dtype = "int16") #移動する人間の声ではうなりが発生する．位置推定に使えそう．
     elif samplewidth == 4:
-        data = np.frombuffer(wav.readframes(nframes), dtype='int32').copy().reshape([nchannels, nframes], order='F').mean(axis=0, dtype = "int32")
+        data = np.frombuffer(wav.readframes(nframes), dtype='int32')[whichchannel::nchannels].copy()
     else:
         #https://qiita.com/Dsuke-K/items/2ad4945a81644db1e9ff
         print("Sample width : ", samplewidth)
